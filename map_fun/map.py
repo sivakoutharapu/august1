6a04db9fe808f994fa31a070591dfcf5416250ec
sivarama krishna <koutharapusivaram.marolix@gmail.com>
2023-09-12 20:50:31 +0530

    map fun

---
+++ map_fun/map.py
@@ -0,0 +1,21 @@
+# Ex-1
+# print('Convert string to integer given list:')
+# list_str = ['9', '7', '6', '2', '3', '11', '21', '66', '24']
+# print(f'string: {list_str}')
+# list_int = list(map(int, list_str))
+# print(f'integer: {list_int}')
+
+# Ex-2
+# print('square of given numbers:')
+# given_lst = [1, 2, 3, 4, 5]
+# sqr_num = list(map(lambda sqr: sqr*sqr,given_lst))
+# print(sqr_num)
+
+# Ex-3
+# print('find len of names above 5 and covert uppercase or else covert capitalize:')
+# names_lst = ['ravi', 'suresh', 'ram', 'sivaram', 'ravi chandra','raman','sri']
+# result = list(map(lambda names: names.upper() if len(names)>5 else names.capitalize(),names_lst))
+# print(result)
+
+
+
