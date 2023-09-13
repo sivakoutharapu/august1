8a237efece848503ba1326962891cbbc4219fde4
sivarama krishna <koutharapusivaram.marolix@gmail.com>
2023-09-13 14:12:47 +0530

    nested condi

---
+++ nested conditions/nested.py
@@ -0,0 +1,32 @@
+# Ex-1
+# print('for loop nested condi:')
+# total1 = 10
+# total2 = 0
+# for i in range(2, 5):
+#     for j in range(i):
+#         total1 = total1 - 1
+#     total2 = total2 + i
+# print("Total1: " + str(total1))
+# print("Total2: " + str(total2))
+
+
+# Ex-2
+# print('if else nested cond:')
+# matches_won = 10
+# goals = 21
+# if matches_won > 8:
+#     if goals > 18:
+#         print("Hurray")
+#     else:
+#         print('less goals')    
+#     print("Winner")
+
+# Ex-3
+# print('def using nested:')
+# def power_num(n):
+#     def power(x):
+#         return n**x
+#     return power
+# for i in range(2,11):
+#     result_power = power_num(i)
+#     print(result_power(3))
