2276c96423fb2f7c3af230e03a3bf95e3dd33207
sivarama krishna <koutharapusivaram.marolix@gmail.com>
2023-09-18 13:51:44 +0530

    floor,ceil,sqrt

--- floor,ceil,sqrt/floor_ceil.py
+++ floor,ceil,sqrt/floor_ceil.py
@@ -15,3 +15,16 @@ print ("math.ceil(300.16) : ", math.ceil(300.16))   # ouput: 301
 print ("math.ceil(300.72) : ", math.ceil(300.72))   # ouput: 301
 
 
+
+# The math.sqrt() method returns the square root of a number.
+# Note: The number must be greater than or equal to 0.
+# If the number is less than 0, it returns a "ValueError". 
+# If the value is not a number, it returns a "TypeError"
+
+print(math.sqrt(9))   # output: 3
+print(math.sqrt(6.6)) # output: 2.569046515733026
+print(math.sqrt(3))   # output: 1.7320508075688772
+# print(math.sqrt(-2))   # output: value error
+# print(math.sqrt('g'))  # output: type error
+
+
