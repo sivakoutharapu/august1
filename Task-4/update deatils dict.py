f4647bed09a2750b8f33b85832cd0c98e420ba88
sivarama krishna <koutharapusivaram.marolix@gmail.com>
2023-10-16 11:52:50 +0530

    16/10

--- Task-4/update deatils dict.py
+++ Task-4/update deatils dict.py
@@ -3,7 +3,7 @@ employ_details = {
     101: {'Name': 'siva', 'EmpID': 101, 'Domain': 'python', 'Email': 'sivaram@gmail.com'}, 
     102: {'Name': 'rama', 'EmpID': 102, 'Domain': '.net', 'Email': 'rama123@gmail.com'},
     103: {'Name': 'krishna', 'EmpID': 103, 'Domain': 'java', 'Email': 'krishna1223@gmail.com'},
-    }
+}
 
 # employ_details = {}
 
@@ -27,32 +27,36 @@ def add_employ_details():
 
 print(employ_details)
 
-check_add_remove_update = input('Enter check or add or remove or update details: ')
+while True:
+    check_add_remove_update_filterdetails = input('Enter option check/add/remove/update : ')
 
-if check_add_remove_update == 'check':
-    given_empid = eval(input('Check empid: '))
-    if given_empid in employ_details:
-        print(employ_details[given_empid])
-    else:
-        print('Employee details not found')
-elif check_add_remove_update == 'add':
-    add_employ_details()
-    print(len(employ_details))
-elif check_add_remove_update == 'remove':
-    given_empid = eval(input('Remove empid: '))
-    if given_empid in employ_details:
-        del employ_details[given_empid]
+    if check_add_remove_update_filterdetails == 'check':
+        given_empid = eval(input('Check empid: '))
+        if given_empid in employ_details:
+            print(employ_details[given_empid])
+        else:
+            print('Employee details not found')
+    elif check_add_remove_update_filterdetails == 'add':
+        add_employ_details()
+        print(len(employ_details))
         print(employ_details)
+    elif check_add_remove_update_filterdetails == 'remove':
+        given_empid = eval(input('Remove empid: '))
+        if given_empid in employ_details:
+            del employ_details[given_empid]
+            print(employ_details)
+        else:
+            print('Employee details not found')
+    elif check_add_remove_update_filterdetails == 'update':
+        given_empid = eval(input('Update empid: '))
+        
+        if given_empid in employ_details:
+            key_name = eval(input('Enter key name: '))
+            update_value = eval(input('Enter the Update value: '))
+            employ_details[given_empid][key_name] = update_value
+            print(employ_details[given_empid])  
+    elif check_add_remove_update_filterdetails == 'exit':
+        print('Exiting program. Good bye!!!')
+        break     
     else:
-        print('Employee details not found')
-elif check_add_remove_update == 'update':
-    given_empid = eval(input('Update empid: '))
-    
-    if given_empid in employ_details:
-        key_name = eval(input('Enter key name: '))
-        update_value = eval(input('Enter the Update value: '))
-        employ_details[given_empid][key_name] = update_value
-        print(employ_details[given_empid])
-            
-else:
-    print('Enter correct value')
+        print('Choose correct option check-add-remove-update-filter')
---
+++ calculator/cal.py
@@ -0,0 +1,13 @@
+num1 = int(input('enetr the value: '))
+b = input('enter the cal symbol: ')
+num2 = int(input('enetr the value: '))
+
+if b == '+':
+    print(num1+num2)
+elif b == '-':
+    print(num1-num2)
+elif b == '*':
+    print(num1*num2)
+elif b == '/':
+    print(num1/num2)
+            
